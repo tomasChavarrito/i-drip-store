20793e16e5b9c97e7fdb1c2199c09ca10caf3c29
Tomas Chavarrito <tomichava2018@gmail.com>
2022-08-29 18:08:35 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Navbar from './components/navegacion/Navbar'
+ import Inicio from './components/pages/Inicio'
+ import Phones from './components/pages/Phone'
+ import Items from './components/pages/Items'
 
-function App() {
-  return (
+function App(){
+
+  return(
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+
+        <Navbar/>
+
+      </Router>
+
+
     </div>
   );
 }
---
+++ src/components/pages/Inicio.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const inicio = () => {
+  return (
+    <div>Pagina Principal</div>
+  )
+}
+
+export default inicio
